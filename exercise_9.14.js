19c975aaafef24ec8d4fdde820552c50a441cc7b
Nicolas Alberga <nicolasalberga@Nicolass-MacBook-Air.local>
2019-09-14 13:18:20 -0400

    first exercise

---
+++ exercise_9.14.js
@@ -0,0 +1,37 @@
+
+var name = ["sarah","bob","Henry","Katie"];
+var age = [32,19,22,9];
+var x = function(age)  {
+if x >= 16 {
+}
+ return true;
+console.log(["name","is old enough to drive"]);
+
+else:
+  return false;
+console.log(["name","cannot drive"]);
+
+if x >= 21 {
+ return true;
+ console.log(["name","is old enough to drink"]);
+
+else:
+  return false;
+  console.log(["name","cannot drink"]);
+if x > 25 {
+ return true;
+ console.log(["name"," is old enough to rent a car"]);
+
+else:
+ return false; 
+ console.log(['name',"cannot rent a car"]);
+}
+
+if x < 10; {
+ return true;
+ console.log(["name","can play in the McDo ball pit"]);
+ return false;
+ console.log(["name","cannot play in the McDo ball pit']);
+}
+}
+
