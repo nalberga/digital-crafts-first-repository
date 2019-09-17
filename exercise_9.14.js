f7b311779272655921a1cbd79c97c4c4704e7dd2
Nicolas Alberga <nicolasalberga@Nicolass-MacBook-Air.local>
2019-09-17 18:50:10 -0400

    updates

--- exercise_9.14.js
+++ exercise_9.14.js
@@ -1,37 +1,99 @@
+// function should test if they are old enough to drive, which is 16
 
-var name = ["sarah","bob","Henry","Katie"];
-var age = [32,19,22,9];
-var x = function(age)  {
-if x >= 16 {
-}
- return true;
-console.log(["name","is old enough to drive"]);
+function checkDrvAge( age ) {
 
-else:
-  return false;
-console.log(["name","cannot drive"]);
-
-if x >= 21 {
- return true;
- console.log(["name","is old enough to drink"]);
-
-else:
-  return false;
-  console.log(["name","cannot drink"]);
-if x > 25 {
- return true;
- console.log(["name"," is old enough to rent a car"]);
-
-else:
- return false; 
- console.log(['name',"cannot rent a car"]);
+    if ( age >= 16 ) {        
+        return true;
+    } else {
+        return false;
+    }
 }
 
-if x < 10; {
- return true;
- console.log(["name","can play in the McDo ball pit"]);
- return false;
- console.log(["name","cannot play in the McDo ball pit']);
-}
+
+// function should test if they are old enough to drink, which is 21
+
+function checkDrkAge( age ) {
+
+    if ( age >= 21 ) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+
+// function should test if they are old enough to rent a car, which is 25
+
+function checkRntAge( age ) {
+
+    if ( age >= 25 ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// function should test if they can play in the McDonalds ball pit, in which they have to be UNDER 10 years old
+
+function checkBalPit( age ) {
+
+    if ( age < 10 ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var sarahAge = 32;
+var bobAge = 19;
+var henryAge = 22;
+var katieAge = 9;
+
+var arrayOfResults = [];
+
+
+// run checks on all conditions (pass in name and age per person)
+
+function checkAgeCond( name, age ) {
+    
+    // check driving age of person    
+
+    if ( checkDrvAge( age ) == true ) {
+        arrayOfResults.push( name + " can drive!" );
+    } else {
+        arrayOfResults.push( name + " cannot drive! ");
+    }
+
+    // check drinking age
+
+    if ( checkDrkAge( age ) == true ) {
+        arrayOfResults.push( name + " can drink!");
+    } else {
+        arrayOfResults.push( name + " cannot drink!");
+    }
+
+    // check car rental age
+
+    if ( checkRntAge( age ) == true) {
+        arrayOfResults.push( name + " can rent a car!");
+    } else {
+        arrayOfResults.push( name + " cannot rent a car!");
+    }
+
+    // check ball pit age
+
+    if ( checkBalPit( age ) == true) {
+        arrayOfResults.push( name + " can enter the ball pit!");
+    } else {
+        arrayOfResults.push( name + " is too old for the ballpit!");
+    }
+    
+}
+
+checkAgeCond ( "Sarah", sarahAge );
+checkAgeCond ( "Bob", bobAge );
+checkAgeCond ( "Henry", henryAge );
+checkAgeCond ( "Katie", katieAge );
+
+console.log( arrayOfResults );
--- js101/exercises/level5.js
+++ js101/exercises/level5.js
@@ -1,19 +1,24 @@
 function lvl5exercise1 () {
   // Push the string "hello" into the array and return it.
   var arr = [1, 'adam']
+ return arr.push("hello");
 }
 
 function lvl5exercise2 () {
   // Remove the last element from the array and return it
   var arr = [1, 'adam']
+  return arr.pop();
 }
+
 
 function lvl5exercise3 () {
   // Return the length of the array
   var arr = [1, 'adam']
+console.log (arr.length);
 }
 
 function lvl5exercise4 () {
   // Return the index of "adam" in the array
   var arr = [1, 'adam']
+    return arr.indexof('adam');
 }
